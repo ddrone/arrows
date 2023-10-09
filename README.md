1507716034931699280eb710376bfa2dd51a90ed
Andrew Shulaev <ash.drone@gmail.com>
2023-10-09 12:20:57 +0100

    Implement a toggle

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Arrows
 
 - [x] Copy the necessary emojis in the project
-- [ ] Create a toggle to select one or more arrows at a time
+- [x] Create a toggle to select one or more arrows at a time
+- [ ] Have multiple toggles in the editor, growing the array automatically on modifying arrows close to the end
+- [ ] Create arrows viewer component, just showing them statically at the moment
+- [ ] Create simple arrow transformations: breaking the sequence with and without repetitions
 - [ ] Blank Vite project has a bunch of recommendations in README.md on what to do before deployment, check them out before deploying it anywhere
--- src/PatternEditor.tsx
+++ src/PatternEditor.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
+import './PatternEditor.css';
+import { classNames } from "./utils/conditionalClasses";
+import { replacing } from "./utils/array";
+
 const downLeft = '↙️';
 const upLeft = '↖️';
 const center = '🟧';
 const upRight = '↗️';
 const downRight = '↘️';
 
-const arrows = [
+const arrowChars = [
   downLeft,
   upLeft,
   center,
@@ -12,8 +18,33 @@ const arrows = [
   downRight
 ];
 
+const initialState = arrowChars.map(() => false);
+
+function ArrowSelector() {
+  const [arrows, setArrows] = useState(initialState);
+
+  function renderArrow(active: boolean, i: number) {
+    return (
+      <button
+        onClick={() => setArrows(replacing(arrows, i, !active))}
+        className={classNames({
+          hidden: !active
+        })}
+      >
+        {arrowChars[i]}
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      {arrows.map(renderArrow)}
+    </div>
+  );
+}
+
 function PatternEditor() {
-  return arrows.join('');
+  return <ArrowSelector />;
 }
 
 export default PatternEditor
