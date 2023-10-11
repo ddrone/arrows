97ba3dfba836814782ef9993802ecf69e41bd28e
Andrew Shulaev <ash.drone@gmail.com>
2023-10-11 20:47:14 +0100

    Implement pattern type selector

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
 - [x] Add bootstrap to the project
 - [x] Put the editor in a column, having some random text on the right to it
 - [x] Parameterize editor component by the arrows, making it possible to have doubles and singles editors at the same time. Add doubles editor to the second column
-- [ ] Create pattern types, single and double with identificator and set of arrow props to render
-- [ ] Move the state of pattern editor upward
-- [ ] Add a type chooser, which should change the type of the editor. Should show a prompt before allowing to change the type if the pattern is non-empty.
+- [x] Create pattern types, single and double with identificator and set of arrow props to render
+- [x] Add a type chooser, which should change the type of the editor
+- [ ] Move the state of pattern editor upward, make sure that the current bug is fixed
+- [ ] Implement a prompt before allowing to change the type if the pattern is non-empty.
 - [ ] Implement saving patterns (via downloading JSON file)
 - [ ] Implement loading patterns (via file input? Figure out a way)
 - [ ] Create arrows viewer component, just showing them statically at the moment
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import PatternEditor, { ArrowProps } from "./PatternEditor"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useState } from "react";
 
 const downLeft = '↙️';
 const upLeft = '↖️';
@@ -22,13 +23,55 @@ const arrowChars: ArrowProps[] = [
   simple(downRight)
 ];
 
+interface PatternType {
+  id: string;
+  description: string;
+  arrowChars: ArrowProps[];
+}
+
 const doubleChars = arrowChars.concat(arrowChars);
+
+const patternTypes: PatternType[] = [
+  {
+    id: 'pump-double',
+    description: 'Double',
+    arrowChars: doubleChars
+  },
+  {
+    id: 'pump-single',
+    description: 'Single',
+    arrowChars: arrowChars
+  },
+];
+
+interface EWTSProps {
+  patternTypes: PatternType[]
+}
+
+function EditorWithTypeSelector(props: EWTSProps) {
+  const [patIndex, setPatIndex] = useState<number>(0);
+  const patType = props.patternTypes[patIndex];
+
+  return (
+    <>
+      <select value={patIndex} onChange={e => setPatIndex(Number(e.target.value))}>
+        {props.patternTypes.map((p, index) => (
+          <option value={index} key={index}>{p.description}</option>
+        ))}
+      </select>
+      <PatternEditor arrowChars={patType.arrowChars} />
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="container">
       <h1>Five arrows (mostly ten, actually)</h1>
       <div className="row">
+        <div className="col">
+          <EditorWithTypeSelector patternTypes={patternTypes} />
+        </div>
         <div className="col">
           <PatternEditor arrowChars={arrowChars} />
         </div>
